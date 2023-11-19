d8f7d4919e91a5e80778d81b105fc3f153a7e405
maxcc1998 <maxwell.c.cummings@gmail.com>
2023-11-19 15:59:38 -0500

    Added modal to create new job - still needs some work

---
+++ app/scenes/GroupsHomePage/components/CreateNewGroupModal.js
@@ -0,0 +1,136 @@
+import React from "react";
+import _ from "lodash";
+import moment from "moment";
+
+import { withApollo } from 'react-apollo';
+import { withRouter, Link} from 'react-router-dom';
+
+import injectSheet from 'react-jss';
+
+import Modal from 'react-modal';
+
+import DataService from '../../../services/DataService';
+
+import COMMON from "../../../common/index";
+import StandardButton from "../../../components/StandardButton";
+import StandardInput from "../../../components/StandardInput";
+import EditPortfolioModalHeader from "../../UserPortfolio/components/EditPortfolioModalHeader";
+import StandardSelect from "../../../components/StandardSelect";
+import axios from "axios";
+import Select from "react-select/creatable";
+import {companyCustomSearch} from "../../../common/helpers";
+import CompanySelect from "../../../components/CompanySelect";
+
+const Styles = {
+    container: {
+        padding: "0",
+        '@media (max-width: 768px)': {
+            padding: "0",
+        },
+    },
+    sectionLabel: {
+        color: COMMON.COLORS.DARK_GREY,
+        marginBottom: "5px",
+        ...COMMON.FONTS.FONT_HEADLINE_BOLD
+    },
+    inputLabel: {
+        color: COMMON.COLORS.DARK_GREY,
+        marginTop: "10px",
+        marginBottom: "6px",
+        ...COMMON.FONTS.FONT_FOOTNOTE
+    },
+    ...COMMON.STYLES.PORTFOLIO.PortfolioPageStyles
+};
+
+class CreateNewGroupModal extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            group_name: null,
+            group_logo_url: null,
+            cover_photo_url:null,
+            group_about: null,
+            group_website: null,
+            group_industry_affiliation: null,
+            group_company_affiliation: null,
+            group_role_affiliation: null,
+            group_school_affiliation: null,
+            privacy_setting: null,
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        let { classes, open, onClose, onSubmit, options } = this.props;
+
+        const {
+            group_name,
+            group_logo_url,
+            cover_photo_url,
+            group_about,
+            group_website,
+            group_industry_affiliation,
+            group_company_affiliation,
+            group_role_affiliation,
+            group_school_affiliation,
+            privacy_setting,
+        } = this.state;
+
+
+        return (<div className={classes.container}>
+            <Modal
+                isOpen={open}
+                closeTimeoutMS={COMMON.STYLES.MODAL_TIMING}
+                style={{
+                    overlay: {
+                        ...COMMON.STYLES.STYLE_MODAL_OVERLAY
+                    },
+                    content: {
+                        ...COMMON.STYLES.EDIT_PORTFOLIO_MODAL.MODAL_CONTAINER,
+                        maxHeight: COMMON.STYLES.EDIT_PORTFOLIO_MODAL.DEFAULT_MODAL_MAX_HEIGHT,
+                        height: "fit-content"
+                    }
+                }}
+            >
+                <div style={{height: "100%", overflow: "hidden"}}>
+                    <div style={{display: "flex", flexDirection: "column", height: "100%", overflow: "hidden", maxHeight: COMMON.STYLES.EDIT_PORTFOLIO_MODAL.DEFAULT_MODAL_MAX_HEIGHT,}}>
+                        <div style={{flex: "0 0 51px", padding: "15px 25px", borderBottom: `1px solid ${COMMON.COLORS.COLOR_BORDER_GREY}`}}>
+                            <EditPortfolioModalHeader title={"Create Group"} onClose={onClose} />
+                        </div>
+
+                        <div style={{flex: 1, padding: "20px 25px", overflow: "scroll"}}>
+                            <div className={classes.inputLabel}>Group name</div>
+                            <StandardInput placeholder={"Ex: 2023 Google Associate Product Managers class of 2023"} value={group_name} update={(v) => (this.setState({group_name: v}))}/>
+
+                            <div className={classes.inputLabel}>Company affiliation</div>
+                            <CompanySelect options={options} onChange={(group_company_affiliation) => {
+                                this.setState({group_company_affiliation});
+                            }}/>
+
+                            <div className={classes.inputLabel}>Group description</div>
+                            <StandardInput placeholder={"Ex: This group is for all current Google APMs that started in 2023"} value={group_about} update={(v) => (this.setState({group_about: v}))}/>
+
+
+                        </div>
+                        <div style={{flex: "0 0 54px", textAlign: "right", padding: "13px 25px",  borderTop: `1px solid ${COMMON.COLORS.COLOR_BORDER_GREY}`}}>
+                            <StandardButton label={"Save"} size={"S"} onClick={() => {
+                                onSubmit ? onSubmit({group_name, group_company_affiliation, group_about}) : null;
+                                onClose ? onClose() : null;
+                            }}/>
+                        </div>
+                    </div>
+
+                </div>
+            </Modal>
+        </div>)
+    }
+
+}
+
+export default withApollo(withRouter(injectSheet(Styles)(CreateNewGroupModal)));
+
--- app/scenes/GroupsHomePage/index.js
+++ app/scenes/GroupsHomePage/index.js
@@ -16,6 +16,11 @@ import CompanyService from "../../services/CompanyService";
 import JobsService from "../../services/JobsService";
 import StandardButton from "../../components/StandardButton";
 import NavBar from "../../components/NavBar";
+import {COLOR_WHITE} from "../../common/colors";
+import {COLORS} from "../../common/colors"
+import Lottie from "react-lottie";
+import loadingCircles from "../../common/lottie/loading-circles";
+import CreateNewGroupModal from "./components/CreateNewGroupModal";
 
 const Styles = {
     container: {
@@ -24,7 +29,26 @@ const Styles = {
             padding: "0 20px",
         },
     },
+    groupBarContainerHeader: {
+        display: "flex",
+        width: "100%",
+        paddingLeft: "25px",
+        //paddingTop: "20px",
+        height:"70px",
+        alignItems: "center",
+        alignSelf: "stretch",
+        background: COLOR_WHITE
+    },
     ...COMMON.STYLES.GENERAL.NavigationStyles,
+};
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingCircles,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
 };
 
 class GroupsHomePage extends React.Component {
@@ -33,6 +57,11 @@ class GroupsHomePage extends React.Component {
         super(props);
 
         this.state = {
+            options: [
+                { value: 'chocolate', label: 'Chocolate' },
+                { value: 'strawberry', label: 'Strawberry' },
+                { value: 'vanilla', label: (<div><img src={"/img/jayson.png"} style={{height: "20px", width: "20px", borderRadius: "4px", marginRight: "5px"}}/>Chocolate </div>) }
+            ],
             openCreateNewGroupModal: false,
             openFindGroupsModal: false,
             openGroupActionsModal: false,
@@ -55,7 +84,7 @@ class GroupsHomePage extends React.Component {
             client,
             user_id: params.user_id
         }).then((users_groups) => {
-            console.log("LOADED USERS GROUPS");
+            console.log("LOADED USERS GROUPS", users_groups);
             users_groups = users_groups ? users_groups.sort ((a,b) =>{
                 let nameA = a.status_id || "";
                 let nameB = b.status_id || "";
@@ -139,7 +168,7 @@ class GroupsHomePage extends React.Component {
 
     render() {
         let { classes, client, match: { params } } = this.props;
-        let { users_groups, openCreateNewGroupModal, openFindGroupsModal, openGroupActionsModal, selectedGroup } = this.state;
+        let { users_groups, groups_map, openCreateNewGroupModal, options, openFindGroupsModal, openGroupActionsModal, selectedGroup } = this.state;
         console.log(this.state);
 
         return (
@@ -147,22 +176,72 @@ class GroupsHomePage extends React.Component {
                 <div className={classes.masterNavContainer}>
                     <NavBar/>
                 </div>
-                <div className={classes.masterBodyContainer}>
-                    <div style={{display: "flex"}} className={classes.container}>
-                        <div className={mc(classes.mainContainer)}>
-                            <div className={mc(classes.rowHeaderSuperContainer)}>
-                                <div className={mc(classes.rowContainerHeader)} style={{flex: 1}}>
-                                    Your Groups
+                <div className={classes.masterBodyContainer} style={{width: "100%", paddingLeft: "50px", paddingRight: "50px",  paddingTop: "50px"}}>
+                    <div className={classes.groupBarContainerHeader}>
+                        <div style={{display: "flex", width: "100%", alignItems: "center", paddingTop: "0px" }} >
+                            <div className={mc(classes.rowContainerHeader)} style={{flex: 1, fontSize: "16px", color: COLORS.G_900}}>
+                                Your Groups
+                            </div>
+                            <div style={{textAlign: "right", marginRight: "25px"}}>
+                                <div style={{flex: "0 0 58x", lineHeight: "initial", marginRight: "5px", display: "inline-block"}} id="find-groups">
+                                    <StandardButton secondary={true} label={"Find Groups"} onClick={() => (this.setState({openFindGroupsModal: true}))}/>
                                 </div>
-                                <div style={{flex: "0 0 58x", lineHeight: "initial", marginTop: "6px"}} id="add-saved-job">
+                                <div style={{flex: "0 0 58x", lineHeight: "initial", display: "inline-block"}} id="create-new-group">
                                     <StandardButton label={"Create Group"} onClick={() => (this.setState({openCreateNewGroupModal: true}))}/>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div className={mc(classes.mainContainer)}>
+                        <div>{users_groups && users_groups.length && !this.state.loading_groups && !this.state.loading_saved_jobs && !this.state.loading_users_groups ? <div>
+                            {users_groups.map((user_group) => {
+                                console.log("USER GROUP GROUP", user_group)
+                                let group = groups_map ? ((groups_map[user_group.group_id] || {})) : {};
+                                let group_name = group.group_id && group_map ? (group_map[group.group_id] || {}).group_name : group.group_name
+
+                                if (!groups_map)
+                                    return;
+                                return (<div className={mc(classes.savedJobRow)} key={user_group.group_id}>
+                                    <div className={mc(classes.jobTitle)}
+                                         style={{flex: 1}}
+                                         onClick={() => {
+                                             this.setState({selectedGroup: user_group});
+                                         }}
+                                    >
+                                        {(groups_map[user_group.group_id] || {}).group_name}
+                                    </div>
+                                </div>)
+                            })}
+                        </div> : (this.state.loading_groups || this.state.loading_users_groups ) ? <div>
+                            <div style={{padding: "20px", textAlign: "center"}}>
+                                <div style={{textAlign: "center"}}>
+                                    <Lottie options={defaultOptions}
+                                            height={96}
+                                            width={96}/>
+                                </div>
+                                <div>Loading Groups</div>
+                                <div>One Moment Please</div>
+                            </div>
+                        </div> : <div>
+                            <div style={{padding: "20px", textAlign: "center"}}>
+                                <div style={{fontSize: "14px"}}>You haven't joined any groups yet! Click below to create a new group</div>
+                                <div style={{marginTop: "20px"}}>
+                                    <StandardButton label={"Create Group"} onClick={() => (this.setState({openCreateNewGroupModal: true}))}/>
+                                </div>
+                            </div>
+                        </div>
+                        } </div>
+                    </div>
+                    <CreateNewGroupModal open={openCreateNewGroupModal} options={options} onSubmit={({group_name, group_about, group_company_affiliation}) => {
+                        console.log("Submit", group_name, group_about, group_company_affiliation);
+
+                        GroupService.addGroup(
+                            client, group_name, group_about, group_company_affiliation
+                        )
+                    }} onClose={() => (this.setState({openCreateNewGroupModal: false}))}/>
                 </div>
             </div>
-        );
+        )
     }
 }
 
